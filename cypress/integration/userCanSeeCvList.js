42aaffdcc0658fc313868727eeae23eafadf649e
Philip Gaunitz <fluffen@pgaunitz.se>
2020-02-23 20:05:22 +0100

    updated cv layout

--- cypress/integration/userCanSeeCvList.js
+++ cypress/integration/userCanSeeCvList.js
@@ -4,28 +4,24 @@ describe('User can see CV', () => {
       cy.get('#cv-tab').click();
     })
   
-    it('displays Education', () => {
+    it('displays Work ', () => {
       cy.get('#cv-1').within(() => {
         cy.get(('.image')).should('exist');
-        cy.get('.ui.header').should('contain', 'Education');
-        cy.get('.description').should('contain', 'Degree');
+        cy.get('.ui.header').should('contain', 'Work experience');
+        cy.get('.date').should('contain', 'Year');
+        cy.get('.description').should('contain', 'Description');
       })
     });
   
-    it('displays job', () => {
+    it('displays School', () => {
       cy.get('#cv-2').within(() => {
         cy.get(('.image')).should('exist');
-        cy.get('.ui.header').should('contain', 'Work experience');
-        cy.get('.description').should('contain', 'Job');
+        cy.get('.ui.header').should('contain', 'Education');
+        cy.get('.date').should('contain', 'Year');
+        cy.get('.description').should('contain', 'Description');
       })
     });
-    it('displays other xp', () => {
-      cy.get('#cv-3').within(() => {
-        cy.get(('.image')).should('exist');
-        cy.get('.ui.header').should('contain', 'Other');
-        cy.get('.description').should('contain', 'Other');
-      })
-    });
+   
   
   
      
--- src/CvCard.jsx
+++ src/CvCard.jsx
@@ -9,7 +9,21 @@ const CvCard = ({ cv }) => {
         </div>
         <div className="content">
           <h3 className="ui header">{cv.name}</h3>
+          <h4 className="ui sub header">{cv.place}</h4>
+          <div className="meta">
+            <span className="date">{cv.date}</span>
+          </div>
           <div className="description">{cv.description}</div>
+          <h4 className="ui sub header">{cv.place2}</h4>
+          <div className="meta">
+            <span className="date">{cv.date2}</span>
+          </div>
+          <div className="description">{cv.description2}</div>
+          <h4 className="ui sub header">{cv.place3}</h4>
+          <div className="meta">
+            <span className="date">{cv.date3}</span>
+          </div>
+          <div className="description">{cv.description3}</div>
         </div>
       </div>
     </>
